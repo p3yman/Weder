2ec60cd20c24e158d2d7ead35a4cc828405d5ee5
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-01-24 09:22:59 +0300

    Add 20 default settings

---
+++ src/configs/cities.js
@@ -0,0 +1,93 @@
+// List of cities
+export default [
+  {
+    id: 3169070,
+    name: "Roma",
+    country: "IT",
+    coord: {
+      lon: 12.4839,
+      lat: 41.894741
+    }
+  },
+  {
+    id: 5128638,
+    name: "New York",
+    country: "US",
+    coord: {
+      lon: -75.499901,
+      lat: 43.000351
+    }
+  },
+  {
+    id: 292223,
+    name: "Dubai",
+    country: "AE",
+    coord: {
+      lon: 55.304722,
+      lat: 25.258169
+    }
+  },
+  {
+    id: 1609350,
+    name: "Bangkok",
+    country: "TH",
+    coord: {
+      lon: 100.51667,
+      lat: 13.75
+    }
+  },
+  {
+    id: 6455259,
+    name: "Paris",
+    country: "FR",
+    coord: {
+      lon: 2.35236,
+      lat: 48.856461
+    }
+  },
+  {
+    id: 1880252,
+    name: "Singapore",
+    country: "SG",
+    coord: {
+      lon: 103.850067,
+      lat: 1.28967
+    }
+  },
+  {
+    id: 1733046,
+    name: "Kuala Lumpur",
+    country: "MY",
+    coord: {
+      lon: 101.686531,
+      lat: 3.14309
+    }
+  },
+  {
+    id: 1850147,
+    name: "Tokyo",
+    country: "JP",
+    coord: {
+      lon: 139.691711,
+      lat: 35.689499
+    }
+  },
+  {
+    id: 112931,
+    name: "Tehran",
+    country: "IR",
+    coord: {
+      lon: 51.421509,
+      lat: 35.694389
+    }
+  },
+  {
+    id: 745044,
+    name: "Istanbul",
+    country: "TR",
+    coord: {
+      lon: 28.949659,
+      lat: 41.01384
+    }
+  }
+];
